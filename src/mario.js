86ed4f86ab2d2f584ffd774db28d406c108c51d0
Max <themaxaxis@gmail.com>
2015-08-30 23:28:47 -0400

    allow parallel raise

--- src/mario.js
+++ src/mario.js
@@ -17,11 +17,11 @@ module.exports = function() {
 	this.destpipe = 0;
 	this.path = {
 		x: [-15, 17, 30],
-		y: [34-this.height, 34 - this.height, 10],
+		y: [34 - this.height, 34 - this.height, 10],
 		k: [rules.k, rules.k, rules.k]
 	};
 	this.generateCurve = function() {
-		this.destpipe = random.number(rules.pipes.length);
+		this.destpipe = random.repnumber(rules.pipes.length);
 		var k = rules.k;
 		this.path.k = this.path.k.concat([k, k, k]);
 		var destx = rules.pipes[this.destpipe] + 15;
@@ -30,10 +30,10 @@ module.exports = function() {
 
 		//climax
 		this.path.y.push(3);
-		this.path.x.push(thres/2);
+		this.path.x.push(thres / 2);
 
 		//buffer approach
-		this.path.y.push(rules.water/2);
+		this.path.y.push(rules.water / 2);
 		this.path.x.push(thres);
 
 		//destination
@@ -41,19 +41,19 @@ module.exports = function() {
 		this.path.x.push(destx);
 	};
 	this.tick = function() {
-		if(this.x > this.path.x[1]) this.y = curve(this.x, this.path); // curve if not on deck
-		if(this.x == this.path.x[1] + 10) audio.play("jump");
+		if (this.x > this.path.x[1]) this.y = curve(this.x, this.path); // curve if not on deck
+		if (this.x == this.path.x[1] + 10) audio.play("jump");
 		this.x++;
-		if(this.y < rules.water) setTimeout(this.tick.bind(this), 10);
-		else if(!this.reached){
+		if (this.y < rules.water) setTimeout(this.tick.bind(this), 10);
+		else if (!this.reached) {
 			this.fading = true;
 			audio.play("water");
 			this.fadeOut();
 		}
 	};
-	this.fadeOut = function(){
+	this.fadeOut = function() {
 		this.opacity -= 0.1;
-		if(this.opacity > 0.1) setTimeout(this.fadeOut.bind(this), 50);
+		if (this.opacity > 0.1) setTimeout(this.fadeOut.bind(this), 50);
 		else this.remove = true;
 	};
 	this.begin = function() {
--- src/rand.js
+++ src/rand.js
@@ -1,5 +1,15 @@
+var prev;
 module.exports = {
-	number: function(max){ //returns between 0 and max - 1
-		return Math.floor(Math.random()*max);
+	number: function(max) { //returns between 0 and max - 1
+		return Math.floor(Math.random() * max);
+	},
+	repnumber: function(max) { //same as number but non-repeating
+		var res = Math.floor(Math.random() * max);
+		if (res == prev) {
+			if (res > 0) res -= 1;  //yes very cheap
+			else res = 1;
+		}
+		prev = res;
+		return res;
 	}
 };
