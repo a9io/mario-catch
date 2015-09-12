dc272189b9711958aa264d8788749b44df610f54
Max <themaxaxis@gmail.com>
2015-09-12 16:07:18 -0400

    balance changes

--- src/spawner.js
+++ src/spawner.js
@@ -1,7 +1,7 @@
 var random = require("./rand");
 var rules = require("./rules");
 module.exports = function(time){
-	var val = (random.number(1800) - (time / rules.spawn));
+	var val = (random.number(1800 - (time / rules.spawn)));
 	if (val < 0) val = 0;
 	return 350 + val;
 };
